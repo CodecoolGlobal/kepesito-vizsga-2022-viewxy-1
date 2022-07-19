5706bdc8c3dfe7e05e91abd8c90e2a6aeb5d6d60
Vivien Szarka <kalna.vivien@gmail.com>
2022-07-19 13:16:12 +0200

    NamecardTest excercise second attempt done

--- src/main/java/DataListFilterPage.java
+++ src/main/java/DataListFilterPage.java
@@ -2,16 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 public class DataListFilterPage extends BasePage {
     private final String dataListFilterURL = "https://demo.seleniumeasy.com/data-list-filter-demo.html";
-    private final By nameCards = By.xpath("//*[@class=\"searchable-container\"]");
+    private final By nameCards = By.xpath("//*[@class=\"info-block block-info clearfix\"]");
     private final By names = By.xpath(".//h4");
-    private final By titles = By.xpath(".//p");
 
     public DataListFilterPage(WebDriver driver) {
         super(driver);
@@ -22,13 +19,10 @@ public class DataListFilterPage extends BasePage {
     }
 
     public String[] getNames() {
-
-        List<Map<String, String>> retList = new ArrayList<Map<String, String>>();
         List<WebElement> nameList = driver.findElements(nameCards);
-        List<WebElement> names = driver.findElements(By.xpath("//*[@class=\"info-block block-info clearfix\"]//h4"));
-        String[] nameArray = new String[names.size()];
-        for (int i = 0; i < names.size(); i++) {
-            String name = names.get(i).getText().replace("Name: ", "");
+        String[] nameArray = new String[nameList.size()];
+        for (int i = 0; i < nameList.size(); i++) {
+            String name = nameList.get(i).findElement(names).getText().replace("Name: ", "");
             nameArray[i] = name;
         }
         return nameArray;
